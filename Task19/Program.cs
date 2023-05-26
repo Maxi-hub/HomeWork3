c5d32e1e96c2f9cf7ce3f75d5bdc552d105893b2
Emma <em.lebedeva85@gmail.com>
2023-05-26 23:20:01 +0300

    create Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (10000 > number || number >= 100000)
+{
+   Console.WriteLine("Введено не пятизначное число. Попробуй еще раз"); 
+}
+
+if (10000 <= number || number < 100000)
+{
+    string num = number.ToString();
+
+    if(num[0] == num[4] && num[1] == num[3]){
+
+        Console.WriteLine("Число является полиндромом");
+    }
+        
+    else {
+        Console.WriteLine("Число не является полиндромом");
+    }
+        
+}
+
