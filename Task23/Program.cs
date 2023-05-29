61b397073f392e22bf6a5bca3b112a52c6afd170
Emma <em.lebedeva85@gmail.com>
2023-05-29 11:52:14 +0300

    create Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+
+while(number >= num)
+{
+    int result = num*num*num;
+    Console.WriteLine(result);
+    num++; 
+}
